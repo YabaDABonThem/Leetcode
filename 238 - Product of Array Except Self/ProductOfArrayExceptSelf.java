da96e090e56d79a51a2761c2968b41fd7eb12ce7
Allen Bao <allenbao@uw.edu>
2023-07-02 22:10:06 -0700

    same as last

--- 238 - Product of Array Except Self/ProductOfArrayExceptSelf.java
+++ 238 - Product of Array Except Self/ProductOfArrayExceptSelf.java
@@ -3,32 +3,31 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         // array that contains the product of all nums before the current num
-        int[] productsExceptSelf = new int[nums.length];
-        // int[] afterCurrentNumProducts = new int[nums.length];
+        int[] suffixList = new int[nums.length];
         int lastIndex = nums.length-1;
 
 
         // Note that nums.length is always greater than 2
         // Get an array containing the products from after index i to the end
-        productsExceptSelf[lastIndex] = nums[lastIndex];
+        suffixList[lastIndex] = nums[lastIndex];
         for (int i = lastIndex-1; i > 0; --i) {
-            productsExceptSelf[i] = productsExceptSelf[i+1] * nums[i];
+            suffixList[i] = suffixList[i+1] * nums[i];
         }
         
         
-        int beforeProduct = nums[0];
+        int prefix = nums[0];
         int newProduct;
         for (int i = 1; i <= lastIndex; ++i) {
             if (i != lastIndex) {
-                newProduct = productsExceptSelf[i+1] * beforeProduct;
+                newProduct = suffixList[i+1] * prefix;
             } else {
-                newProduct = beforeProduct;
+                newProduct = prefix;
             }
             
-            beforeProduct *= nums[i];
+            prefix *= nums[i];
             nums[i] = newProduct;
         }
-        nums[0] = productsExceptSelf[1];
+        nums[0] = suffixList[1];
         
         return nums; // I guess we can just modify the original to save space
     }
